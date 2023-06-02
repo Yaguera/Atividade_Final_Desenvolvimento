f49932d05a4c0a2d1dd26e1d5a322f3768f95139
Yago Gomes <yaguamineral@gmail.com>
2023-06-01 22:32:25 -0300

    Adicionado os atributos construtores e metodos da classe Produto e aplicaçao dos mesmos na casse App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Produto produto = new Produto("Coca-cola", 300);
+        System.out.println("Adicionado " + produto.getQuantidade() + " unidades de " + produto.getNome() + " ao estoque.");
+        
+        int addQtd = produto.adicionarQuantidade(100);
+        System.out.println("Foi adicionado " + addQtd + " unidades de " + produto.getNome() + ". Agora temos " + produto.getQuantidade() + " unidades no estoque.");
+        int removeQtd = produto.removerQuantidade(50);
+        System.out.println("Foi removido " + removeQtd + " unidades de " + produto.getNome() + ". Agora temos " + produto.getQuantidade() + " unidades no estoque.");
+        
+
+        
+
     }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -1,1 +1,43 @@
+public class Produto {
+    private String nome;
+    private int quantidade;
 
+    // Construtor da classe
+    public Produto(String nome, int quantidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    // Getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    // Método para adicionar uma quantidade ao produto
+    public int adicionarQuantidade(int quantidade) {
+        this.quantidade += quantidade;
+        return quantidade;
+    }
+
+    // Método para remover uma quantidade do produto
+    public int removerQuantidade(int quantidade) {
+        if (this.quantidade >= quantidade) {
+            this.quantidade -= quantidade;
+        } else {
+            System.out.println("Não é possível remover a quantidade especificada. Quantidade insuficiente em estoque.");
+        }
+        return quantidade;
+    }
+}
