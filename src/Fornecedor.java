1b6d9e4e4e56a614314c0371c5982ed7695da0b9
Lara Sampaio <larafbsampaio2@hotmail.com>
2023-06-02 19:24:34 -0300

    Movi a classe para a pasta src

---
+++ src/Fornecedor.java
@@ -0,0 +1,47 @@
+public class Fornecedor {
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String email;
+
+    public Fornecedor (String nome, String endereco, String telefone, String email) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco () {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail () {
+        return email;
+    }
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    // Método para solicitar o reabastecimento de determinado produto
+    public void solicitarReabastecimento(String produto, int quantidade) {
+        System.out.println ("Solicitar reabastecimento de " + quantidade + " unidades de " + produto + " ao fornecedor " + nome);
+    }
+   // Método para notificar atraso na entrega de um pedido de determinado fornecedor
+   public void notificarAtrasoEntrega (String pedidoID) {
+    System.out.println("Notificar atraso na entrega do pedido " + pedidoID + " ao fornecedor " + nome);
+   }
+    
+} 
